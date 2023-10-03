c5c2761d73d64cd4a9180b2c3aba2c7fb72375f0
Kakaromito <arthur.raposooficial@gmail.com>
2023-10-03 20:06:58 -0300

    Adicionar arquivos de projeto.

---
+++ MVC definitivo/Controllers/AnimesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_definitivo.Models;
+
+namespace MVC_definitivo.Controllers
+{
+    public class AnimesController : Controller
+    {
+        // GET: Animes
+        public ActionResult Index()
+        {
+            return View(AnimesModel.CriarList());
+
+        }
+
+        // GET: Animes/Details/5
+        public ActionResult Details(int id)
+        {
+
+
+            var animes = AnimesModel.CriarList()[id];
+            return View(animes);
+        }
+
+        // GET: Animes/Create
+        public ActionResult Create(int id)
+        {
+            var animes = AnimesModel.CriarList()[id];
+            return View(animes);
+        }
+
+        // POST: Animes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var animes = AnimesModel.CriarList()[id];
+            return View(animes);
+        }
+
+        // POST: Animes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var animes = AnimesModel.CriarList()[id];
+            return View(animes);
+        }
+
+        // POST: Animes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC definitivo/Models/AnimesModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_definitivo.Models
+{
+    public class AnimesModel
+    {
+        public string Nome { get; set; }
+        public string Protagonista { get; set; }
+        public int Episodios { get; set; }
+        public string Lançamento { get; set; }
+
+        public static List<AnimesModel> CriarList()
+        {
+            var list = new List<AnimesModel>();
+            list.Add(new AnimesModel() { Nome = "Samurai-X", Protagonista = "Kenshin Rorouini", Episodios = 94, Lançamento = "10 de Janeiro de 1996" });
+            list.Add(new AnimesModel() { Nome = "Chainsaw Man", Protagonista = "Denji", Episodios = 12, Lançamento = "12 de Outubro de 2022" });
+            list.Add(new AnimesModel() { Nome = "Naruto Shippuden", Protagonista = "Naruto Uzumaki", Episodios = 500, Lançamento = "15 de Fevereiro de 2007" });
+            list.Add(new AnimesModel() { Nome = "Jujutsu Kaisen", Protagonista = "Yujji Itadori", Episodios = 34, Lançamento = "2 de Outubro de 2020" });
+            return list;
+        }
+    }
+}
